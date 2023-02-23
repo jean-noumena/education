04657ebe0763d1bad043d8c7cddd8c45f0c7e220
stuart-noumena <90445361+stuart-noumena@users.noreply.github.com>
2023-02-23 15:29:46 +0000

    Set openapi.yml servers.url correctly according to the environment (#95)
    * Set openapi.yml servers.url correctly according to the environment
    
    * minor url fix for loginRoutes - openapi/openapi.yml

--- base/src/main/kotlin/seed/server/LoginRoutes.kt
+++ base/src/main/kotlin/seed/server/LoginRoutes.kt
@@ -1,8 +1,12 @@
 package seed.server
 
+import org.http4k.core.Body
 import org.http4k.core.ContentType
+import org.http4k.core.HttpHandler
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
+import org.http4k.core.Response
+import org.http4k.core.Status
 import org.http4k.core.then
 import org.http4k.routing.ResourceLoader.Companion.Classpath
 import org.http4k.routing.RoutingHttpHandler
@@ -17,10 +21,26 @@ fun loginRoutes(
     config: IConfiguration,
     authHandler: AuthHandler,
 ): RoutingHttpHandler = routes(
-    "/openapi" bind GET to static(Classpath("/"), "yml" to ContentType.TEXT_YAML),
+    "/openapi/openapi.yml" bind GET to openApi(config),
     "/swagger" bind GET to static(Classpath("/swagger-ui-3.52.3/dist")),
 
     "/auth/login" bind POST to authHandler.login(),
     "/auth/refresh" bind POST to authHandler.refresh(),
     "/auth/logout" bind POST to loginRequired(config).then(authHandler.logout())
 )
+
+fun openApi(config: IConfiguration): HttpHandler {
+    return { _ ->
+        val resourceLoader = Classpath("/")
+        val openApiUrl = resourceLoader.load("openapi.yml")
+        val contents = openApiUrl?.readText()
+        val responseBody = contents!!.replace(
+            "- url: http://localhost:8080",
+            "- url: ${config.apiServerUrl}"
+        )
+
+        Response(Status.OK)
+            .header("Content-Type", ContentType.TEXT_YAML.toHeaderValue())
+            .body(Body(responseBody))
+    }
+}
